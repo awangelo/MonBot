6dfa86476503649305ce37d8e42629225f40a65c
awangelo <anggg.gaio@gmail.com>
2024-10-09 09:55:21 -0300

    defer client disconnect

--- cmd/monbot/main.go
+++ cmd/monbot/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/awangelo/MonBot/internal/bot"
 	"github.com/awangelo/MonBot/internal/handler"
@@ -18,14 +15,10 @@ func main() {
 	}
 	handler.InitHandler(client)
 	handler.LoginByQr(client)
+	defer client.Disconnect()
 
 	utils.ListGroups(client)
 	bot.SendScheduledMessage(client, groupJID, delayMins)
 
-	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-
-	client.Disconnect()
+	utils.PreventExit()
 }
