b27e6f75a54be95e1eb7e4a900adc2e2334bd8de
awangelo <anggg.gaio@gmail.com>
2024-10-09 19:09:32 -0300

    remocao de groupJID para uso como variavel global

--- cmd/monbot/main.go
+++ cmd/monbot/main.go
@@ -9,16 +9,16 @@ import (
 )
 
 func main() {
-	client, groupJID, delayMins, err := bot.InitBot()
+	client, delayMins, err := bot.InitBot()
 	if err != nil {
 		log.Fatalf("Error creating bot: %v", err)
 	}
 	handler.InitHandler(client)
-	handler.LoginByQr(client)
+	utils.LoginByQr(client)
 	defer client.Disconnect()
 
 	utils.ListGroups(client)
-	bot.SendScheduledMessage(client, groupJID, delayMins)
+	bot.SendScheduledMessage(client, delayMins)
 
 	utils.PreventExit()
 }
