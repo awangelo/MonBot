0aeb26dfc046e3e342ffb2e3c1a05320c872d3d8
awangelo <anggg.gaio@gmail.com>
2024-10-14 17:16:55 -0300

    groupJID sendo usando diretamente para mensagens programadas

--- cmd/monbot/main.go
+++ cmd/monbot/main.go
@@ -4,21 +4,29 @@ import (
 	"log"
 
 	"github.com/awangelo/MonBot/internal/bot"
+	"github.com/awangelo/MonBot/internal/config"
 	"github.com/awangelo/MonBot/internal/handler"
 	"github.com/awangelo/MonBot/internal/utils"
 )
 
 func main() {
-	client, delayMins, err := bot.InitBot()
+	// Obter variáveis de ambiente
+	client, groupJID, delayMins, err := config.InitBot()
 	if err != nil {
-		log.Fatalf("Error creating bot: %v", err)
+		log.Fatalf("Erro ao inicializar o bot: %v", err)
 	}
-	handler.InitHandler(client)
-	utils.LoginByQr(client)
+	// Inicializa o handler
+	handler.InitHandler(client, groupJID)
+
+	// Login do bot
+	bot.Login(client)
 	defer client.Disconnect()
 
-	utils.ListGroups(client)
-	bot.SendScheduledMessage(client, delayMins)
+	// Listar grupos
+	bot.ListGroups(client)
+
+	// Enviar mensagem automatica
+	go bot.SendScheduledMessage(client, groupJID, delayMins)
 
 	utils.PreventExit()
 }
