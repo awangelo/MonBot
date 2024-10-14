83aeac4a0a814d9f4fe30d45db47c1469244c045
awangelo <anggg.gaio@gmail.com>
2024-10-14 17:19:03 -0300

    verificacao simplificada do grupo

--- internal/handler/event.go
+++ internal/handler/event.go
@@ -6,38 +6,44 @@ import (
 	"github.com/awangelo/MonBot/internal/bot"
 	"github.com/awangelo/MonBot/internal/utils"
 	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/types"
 	"go.mau.fi/whatsmeow/types/events"
 )
 
-var client *whatsmeow.Client
-
-func eventHandler(evt interface{}) {
-	switch v := evt.(type) {
-	case *events.Message:
-		go utils.InitLogger(v)
-		handleGroupMessage(v)
-	}
-}
-
-func InitHandler(c *whatsmeow.Client) {
-	c.AddEventHandler(eventHandler)
-	client = c
-}
+var (
+	client *whatsmeow.Client
+	group  types.JID
+)
 
 func handleGroupMessage(msg *events.Message) {
-	// Ignorar se não for mensagem de um grupo e se não for uma mensagem de texto
-	if msg.Info.Chat.Server != "g.us" {
+	// Ignorar se nao for mensagem do grupo especifico
+	if msg.Info.Chat != group {
 		return
 	}
 
-	if utils.IsBotMentioned(client, msg) {
+	if bot.IsBotMentioned(client, msg) {
 		log.Println("O bot foi mencionado!")
 		bot.ReplyToMention(client, msg)
 		return
 	}
 
 	if msg.Message.GetConversation()[0] == '!' {
-		bot.HandleCommand(client, msg)
+		HandleCommand(client, msg)
 		return
 	}
+}
+
+func eventHandler(evt interface{}) {
+	switch v := evt.(type) {
+	case *events.Message:
+		utils.LogMessageEvent(v)
+		handleGroupMessage(v)
+	}
+}
+
+// InitHandler inicializa o handler de eventos
+func InitHandler(c *whatsmeow.Client, g types.JID) {
+	c.AddEventHandler(eventHandler)
+	client = c
+	group = g
 }
